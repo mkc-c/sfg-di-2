127d17a3a65c84d9875370719470d121eef7c47d
mkc-c <abdirisak.arr@gmail.com>
2024-02-11 10:35:12 +0000

    setter injected controller example

---
+++ src/main/java/guru/springframework/sgfdi2/controllers/SetterInjectedController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sgfdi2.controllers;
+
+import guru.springframework.sgfdi2.services.GreetingService;
+
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/test/java/guru/springframework/sgfdi2/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,26 @@
+package guru.springframework.sgfdi2.controllers;
+
+import guru.springframework.sgfdi2.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+
+        controller = new SetterInjectedController();
+
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(controller.getGreeting());
+    }
+}
