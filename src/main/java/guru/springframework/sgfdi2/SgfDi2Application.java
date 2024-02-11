c23703583f8026f62f02ce3f9f2f96bc960e1530
mkc-c <abdirisak.arr@gmail.com>
2024-02-11 08:48:47 +0000

    initialize project

---
+++ src/main/java/guru/springframework/sgfdi2/SgfDi2Application.java
@@ -0,0 +1,13 @@
+package guru.springframework.sgfdi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgfDi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgfDi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sgfdi2/SgfDi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sgfdi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfDi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
