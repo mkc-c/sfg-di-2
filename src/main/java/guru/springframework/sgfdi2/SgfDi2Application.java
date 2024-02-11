0184a17d28e3b3a5bbda2e694ed93f0920e8200b
mkc-c <abdirisak.arr@gmail.com>
2024-02-11 10:05:56 +0000

    modify spring application runner to assign spring context and use it to access and call controller instance

--- src/main/java/guru/springframework/sgfdi2/SgfDi2Application.java
+++ src/main/java/guru/springframework/sgfdi2/SgfDi2Application.java
@@ -1,13 +1,21 @@
 package guru.springframework.sgfdi2;
 
+import guru.springframework.sgfdi2.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SgfDi2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SgfDi2Application.class, args);
+		ApplicationContext ctx = SpringApplication.run(SgfDi2Application.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
